f9b8f884a9239cd3ea230a37d6fb25c2dc0d2e42
juliiiscript <valelezca27@gmail.com>
2026-06-23 23:31:32 -0300

    Add files via upload
    entrega proyecto final

---
+++ estudio_db.sql
@@ -0,0 +1,53 @@
+h1 {
+    color: chartreuse;
+    text-align: center;
+    text-decoration: underline;
+}
+
+p {
+    font-size: 18px
+}
+
+h5 {
+    text-align: center;
+    color: orange;
+    font-size: 300%;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    border-radius: 0cqh;
+}
+
+img {
+    text-align: center;
+}
+
+.fjalla-one-regular {
+    font-family: "Fjalla One", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+}
+
+.container {
+    width: 100% padding 10px
+}
+
+@media(min-width:768px) {}
+
+.container {
+    width: 720px
+}
+
+
+.menu {
+    display: flex;
+
+}
+
+@media (min-width: 768px) {
+    .container {
+        width: 960px;
+    }
+}
+
+.sidebar {
+    display: block;
+}
---
+++ guardar.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="guardar.php" method="GET">
+    <input type="text" name="nombre" placeholder="Tu nombre" required><br><br>
+    <input type="email" name="email" placeholder="Tu email" required><br><br>
+    <textarea name="mensaje" placeholder="Escribí algo"></textarea><br><br>
+    <button>Guardar</button>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+</body>
+</html>
